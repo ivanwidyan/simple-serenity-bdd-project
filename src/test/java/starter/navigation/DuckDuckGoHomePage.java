d834096e5d24bc3a7d30eee44d05a8c8a4eda66c
Ivan Widyan <ivan.widyan@gdn-commerce.com>
2019-06-24 21:28:46 +0700

    Config setting & preparations done

--- src/test/java/starter/navigation/DuckDuckGoHomePage.java
+++ src/test/java/starter/navigation/DuckDuckGoHomePage.java
@@ -3,5 +3,6 @@ package starter.navigation;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("https://duckduckgo.com")
-class DuckDuckGoHomePage extends PageObject {}
+@DefaultUrl("https://www.blibli.com")
+class DuckDuckGoHomePage extends PageObject {
+}
--- src/test/java/starter/navigation/NavigateTo.java
+++ src/test/java/starter/navigation/NavigateTo.java
@@ -1,13 +1,28 @@
 package starter.navigation;
-
+import net.serenitybdd.core.SerenitySystemProperties;
+import net.serenitybdd.core.environment.EnvironmentSpecificConfiguration;
+import net.thucydides.core.ThucydidesSystemProperty;
 import net.thucydides.core.annotations.Step;
+import net.thucydides.core.util.EnvironmentVariables;
+import net.thucydides.core.util.SystemEnvironmentVariables;
 
 public class NavigateTo {
+
+    EnvironmentVariables variables = SystemEnvironmentVariables.createEnvironmentVariables();
+
+    String baseUrl = variables.getProperty(ThucydidesSystemProperty.WEBDRIVER_BASE_URL);
+    String testUrl = EnvironmentSpecificConfiguration.from(variables)
+            .getProperty("homepage.url");
+
+    String myCustomProperty = variables.getProperty("serenity.project.name");
 
     DuckDuckGoHomePage duckDuckGoHomePage;
 
     @Step("Open the DuckDuckGo home page")
     public void theDuckDuckGoHomePage() {
-        duckDuckGoHomePage.open();
+        System.out.println("base: " + baseUrl);
+        System.out.println("test: " + testUrl);
+        System.out.println("custom: " + myCustomProperty);
+        duckDuckGoHomePage.openUrl("https://www.blibli.com");
     }
 }
